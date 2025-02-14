105255ab7f3a97e320c9e655d38b1fbb420acd63
fmfahath <fmfahath.dev@gmail.com>
2025-02-14 07:47:56 +0530

    server(backend) --  mongoDB atlas account connected, .env variable added, connection added in server.js,

--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
+import connectDB from './config/mongodb.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ credentials: true }))
 
-app.get('/', (req, res) => res.send("API Working"))
+app.get('/', (req, res) => res.send("API Working on port:4000"))
 
 app.listen(port, () => console.log(`Server started on post:${port}`))
