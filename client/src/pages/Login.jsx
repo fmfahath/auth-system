c2441beb5d4e3a25e10fdd6110459247e65b21d6
fmfahath <fmfahath.dev@gmail.com>
2025-02-17 00:20:56 +0530

    client(frontend) -- onSubmitHandler, useContext added in login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { AppContent } from '../context/AppContext'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
@@ -10,13 +13,48 @@ const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const { backendUrl, setIsLoggedin } = useContext(AppContent)
+
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault();
+            axios.defaults.withCredentials = true;
+
+            if (state === 'Sign Up') {
+                const { data } = await axios.post(backendUrl + '/api/auth/register', { name, email, password })
+
+                if (data.success) {
+                    setIsLoggedin(true);
+                    navigate('/')
+                }
+                else {
+                    toast.error(data.message)
+                }
+            } else {
+                const { data } = await axios.post(backendUrl + '/api/auth/login', { email, password })
+
+                if (data.success) {
+                    setIsLoggedin(true);
+                    navigate('/')
+                }
+                else {
+                    toast.error(data.message)
+                }
+            }
+
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     return (
         <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
             <img src={assets.logo} alt='login-logo' className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' onClick={() => navigate('/')} />
             <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
                 <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
                 <p className='text-center text-sm mb-6 text-gray-400' >{state === 'Sign Up' ? 'Create your account' : 'Login to your account'}</p>
-                <form>
+                <form onSubmit={onSubmitHandler}>
                     {state === 'Sign Up' && (
                         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                             <img src={assets.person_icon} alt="person-icon" />
