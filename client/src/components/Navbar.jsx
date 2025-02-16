4ba4df72d4ea31a483c64c3cfbb48fd5794594fc
fmfahath <fmfahath.dev@gmail.com>
2025-02-16 22:50:47 +0530

    client(frontend) -- lgoin / register form css added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
+
     return (
         <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
             <img src={assets.logo} alt="logo" className='w-28 sm:w-32' />
@@ -16,7 +21,7 @@ const Navbar = () => {
             text-gray-800 
             hover:bg-gray-100 
             transition-all
-            '>Login <img src={assets.arrow_icon} alt='login-icon' /></button>
+            ' onClick={() => navigate('/login')}>Login <img src={assets.arrow_icon} alt='login-icon' /></button>
         </div>
     )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
+
+    const [state, setState] = useState('Sign Up')
+
     return (
-        <div>Login</div>
+        <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+            <img src={assets.logo} alt='login-logo' className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+            <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
+                <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
+                <p className='text-center text-sm mb-6 text-gray-400' >{state === 'Sign Up' ? 'Create your account' : 'Login to your account'}</p>
+                <form>
+                    {state === 'Sign Up' && (
+                        <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                            <img src={assets.person_icon} alt="person-icon" />
+                            <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
+                        </div>
+                    )}
+                    <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                        <img src={assets.mail_icon} alt="person-icon" />
+                        <input className='bg-transparent outline-none text-white' type="mail" placeholder='Email' required />
+                    </div>
+                    <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                        <img src={assets.lock_icon} alt="person-icon" />
+                        <input className='bg-transparent outline-none text-white' type="password" placeholder='Password' required />
+                    </div>
+
+                    <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot password ?</p>
+
+                    <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium'>{state}</button>
+
+                </form>
+
+                {state === "Sign Up" ?
+                    (<p className='text-gray-400 text-center text-xs mt-4'>Already have an account? <span className='text-blue-400 cursor-pointer underline'>Login here</span></p>)
+                    :
+                    (<p className='text-gray-400 text-center text-xs mt-4'>Don't have an account? <span className='text-blue-400 cursor-pointer underline'>Register here</span></p>)
+                }
+            </div>
+        </div>
     )
 }
 
