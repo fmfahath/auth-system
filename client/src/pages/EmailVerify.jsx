9d450236fb22e167508c40cc195f92bb6978c8e7
fmfahath <fmfahath.dev@gmail.com>
2025-02-17 03:56:59 +0530

    client(frontend) -- EmailVerify component css added, input field auto focus & move to next fun added

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const EmailVerify = () => {
+
+    const navigate = useNavigate()
+    const inputRef = useRef([])
+
+    const inputHandler = (e, index) => {
+        if (e.target.value.length > 0 && index < inputRef.current.length - 1) {
+            inputRef.current[index + 1].focus()
+        }
+    }
+
     return (
-        <div>EmailVerify</div>
+        <div className='flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400'>
+            <img src={assets.logo} alt='login-logo' className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' onClick={() => navigate('/')} />
+            <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+                <h1 className='text-white text-2xl text-semibold text-center mb-4'>Verify Email</h1>
+                <p className='text-indigo-300 text-center mb-6'>Enter the 6-digit code sent to your email ID</p>
+                <div className='flex justify-between mb-8'>
+                    {Array(6).fill(0).map((_, index) => (
+                        <input
+                            className='w-12 h-12 bg-[#333A5C] mb-8 rounded-md text-white text-center text-xl'
+                            type='text'
+                            maxLength='1'
+                            key={index}
+                            required
+                            ref={e => inputRef.current[index] = e}
+                            onInput={(e) => inputHandler(e, index)}
+                        />
+                    ))}
+                </div>
+                <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer'>Verify Email</button>
+            </form>
+        </div>
     )
 }
 
