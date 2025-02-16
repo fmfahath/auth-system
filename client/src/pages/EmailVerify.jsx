0d97ee74270831c2054e79f71289372952eaf8e8
fmfahath <fmfahath.dev@gmail.com>
2025-02-17 04:19:21 +0530

    client(frontend) -- handlepase-auto input field complete fun added

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -19,13 +19,24 @@ const EmailVerify = () => {
         }
     }
 
+    const handlePaste = (e) => {
+        const paste = e.clipboardData.getData('text')
+        const pasteArray = paste.split('')
+        pasteArray.forEach((char, index) => {
+            if (inputRef.current[index]) {
+                inputRef.current[index].value = char;
+            }
+        })
+    }
+
+
     return (
         <div className='flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400'>
             <img src={assets.logo} alt='login-logo' className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' onClick={() => navigate('/')} />
             <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
                 <h1 className='text-white text-2xl text-semibold text-center mb-4'>Verify Email</h1>
                 <p className='text-indigo-300 text-center mb-6'>Enter the 6-digit code sent to your email ID</p>
-                <div className='flex justify-between mb-8'>
+                <div className='flex justify-between mb-8' onPaste={handlePaste}>
                     {Array(6).fill(0).map((_, index) => (
                         <input
                             className='w-12 h-12 bg-[#333A5C] mb-8 rounded-md text-white text-center text-xl'
