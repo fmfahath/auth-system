efcffd723437abd7d57d94d094adb32c6afbfaef
fmfahath <fmfahath.dev@gmail.com>
2025-02-16 10:11:09 +0530

    client(frontend) -- pages,components, router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold'>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/verify-email' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
     </div>
   )
 }
